f1980c46a3c64610eb91e4adbaefd84e21e8d53a
Felipe França <0double.xp0@gmail.com>
2024-08-10 20:27:32 -0300

    Implementado carrosel e re-desing

--- app.js
+++ app.js
@@ -1,2 +1,24 @@
-let container = document.querySelector('.container')
-let card = container.querySelector('.cards .card')
+const slides = document.querySelectorAll('.slide')
+const dots = document.querySelectorAll('.dots li')
+let slideIndex = 0;
+
+function mostrarSlide(index) {
+    slides.forEach((slides, i) => {
+        slides.classList.toggle('active', i === index);
+        dots[i].classList.toggle('active', i === index);
+    });
+}
+
+function trocarSlides() {
+    slideIndex = (slideIndex + 1) % slides.length;
+    mostrarSlide(slideIndex)
+}
+
+dots.forEach((dot, i) => {
+    dot.addEventListener('click', () => {
+      slideIndex = i;
+      mostrarSlide(slideIndex);
+    });
+  });
+
+setInterval(trocarSlides, 5000)
